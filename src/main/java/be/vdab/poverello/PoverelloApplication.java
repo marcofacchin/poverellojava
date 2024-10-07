60aa23587a46d12d514e68f3620b32d6a6339030
MarcoFacchin <marco.facchin@vdabcampus.be>
2024-10-07 13:10:58 +0200

    aanmaak project

---
+++ src/main/java/be/vdab/poverello/PoverelloApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.poverello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoverelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoverelloApplication.class, args);
+    }
+
+}
