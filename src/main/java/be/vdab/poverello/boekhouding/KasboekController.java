b195b210e2ea1937d9c13178cfbc65808e6fded3
MarcoFacchin <marco.facchin@vdabcampus.be>
2025-05-10 17:59:05 +0200

    crossorigin

--- src/main/java/be/vdab/poverello/boekhouding/KasboekController.java
+++ src/main/java/be/vdab/poverello/boekhouding/KasboekController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 import java.util.stream.Stream;
 
+@CrossOrigin
 @RestController
 @RequestMapping("kasboeken")
 public class KasboekController {
